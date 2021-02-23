6432cbecc6f5b9454ecf4251f1b0d8e2f425d62e
karla-cervantes <cervantes.karla14@gmail.com>
2021-02-22 19:52:19 -0600

    all endpoint are working as requested!

--- api/server.js
+++ api/server.js
@@ -5,14 +5,39 @@ const session = require('express-session');
 const KnexSessionStore = require('connect-session-knex')(session);
 
 // Routers will go here
+const userRouter = require('./users/users-router');
+const authRouter = require('./auth/auth-router');
 
 const server = express();
 
+const config = {
+    name:'sessionId',
+    secret: 'this is a secret, keep it safe',
+    cookie:{
+        maxAge: 1000 * 60 * 60,
+        secure:false,
+        httpOnly:true
+    },
+    resave:false,
+    saveUnitialized:false,
+
+    store: new KnexSessionStore({
+        knex:require('../database/connection'),
+        tablename:'sessions',
+        sidfilename:'sid',
+        createTable:true,
+        clearInterval:1000 * 60 * 60
+    })
+}
+
+server.use(session(config));
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
 // connect Routers to servers here
+server.use('/api/users', userRouter);
+server.use('/api/auth', authRouter);
 
 server.get('/', (req, res) => {
     res.json({ api: "is alive!"});
